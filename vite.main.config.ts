89eab7c49aed59073b772a0e2e670556a3bcd6c5
Horace Liu <im.liuhq@gmail.com>
2025-09-27 22:15:59 +0800

    fix(vite): add path aliases to all config files

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,14 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config
-export default defineConfig({})
+export default defineConfig({
+    resolve: {
+        alias: {
+            '#lib': resolve(__dirname, './lib'),
+            '#main': resolve(__dirname, './main'),
+            '#renderer': resolve(__dirname, './renderer'),
+            '#config': resolve(__dirname, './config'),
+        },
+    },
+})
